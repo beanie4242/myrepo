891140f624fd31d2048ae0d78c4b94a925b1ec09
beanie4242 <kjs12@ic.ac.uk>
2024-10-22 16:19:00 +0100

    Init project XYZ

---
+++ git.R
@@ -0,0 +1,14 @@
+# Generate a new PAT token
+usethis::create_github_token()
+
+# Replace the current PAT token
+gitcreds::gitcreds_set()
+
+# View current settings
+gitcreds::gitcreds_get()
+
+# Creating a new R studio project from one started in github
+usethis::create_from_github(
+  repo_spec = "https://github.com/beanie4242/myrepo.git", # GitHub URL
+  destdir = "~/Documents/GitHub" # Local project location
+)
